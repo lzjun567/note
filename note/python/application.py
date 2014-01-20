eec2c1a916cff38613ce2c88f781b755880106cf
liu <lzjun567@gmail.com>
2014-01-20 18:33:17 +0800

    update

---
+++ note/python/application.py
@@ -0,0 +1,64 @@
+#! /usr/bin/env python
+from wsgiref.simple_server import make_server
+from cgi import parse_qs, escape
+
+html = """
+    <html>
+    <body>
+        <form method="post" action="application.py">
+        <p>
+            Age:<input type="text" name="age">
+        </p>
+        <p>
+        Hobbies:
+        <input name="hobbies" type="checkbox" value="software">Software
+        <input name="hobbies" type="checkbox" value="tunning">Auto Tunning
+        </p>
+        <p>
+        <input type="submit" value="Submit">
+        </p>
+    <p>
+    Age:%s<br>
+    Hobbies:%s
+    </p>
+    </body>
+    </html>
+    """
+
+def application(environ, start_response):
+    #response_body = "the request method was %s" % environ['REQUEST_METHOD']
+    #response_body = response_body*1000
+    #status = '200 OK'
+    #response_headers = [('Content-Type', 'text/plain'),
+    #                    ('Content-Length', str(len(response_body)))]
+
+    #start_response(status, response_headers)
+    #return [response_body]
+    try:
+        request_body_size = int(environ.get('CONTENT_LENGTH', 0))
+    except:
+        request_body_size = 0 
+
+    request_body = environ['wsgi.input'].read(request_body_size)
+    d = parse_qs(request_body)
+
+    age = d.get('age', [''])[0]
+    hobbies = d.get('hobbies',[])
+
+    age = escape(age)
+    hobbies = [escape(hobby) for hobby in hobbies]
+
+    response_body = html % (age or 'Empty',','.join(hobbies or ['No Hobbies']))
+    status = '200 OK'
+    response_headers = [('Content-Type','text/html'),
+                        ('Content-Length', str(len(response_body)))]
+    start_response(status, response_headers)
+    return [response_body]
+
+
+httpd= make_server('localhost',
+                    8051,
+                    application)
+
+#httpd.handle_request()
+httpd.serve_forever()
